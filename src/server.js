4eb52e6ff35553797853c7de70907e6b7e232219
LucasCardoso-tech <lucascardosodemelo08@gmail.com>
2025-03-17 11:07:42 -0300

    server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
+const express = require("express");
+const cors = require("cors");
 
-const express = require('express');
 const app = express();
-const routes = require('./routes');
-const { corsOptions, limiter } = require('./middlewares');
-const logger = require('./config/logs');
 
-app.use(require('cors')(corsOptions));
-app.use(limiter);
 app.use(express.json());
-app.use('/', routes);
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("API está rodando corretamente!");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    logger.info(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+// Exportação necessária para rodar no Vercel
+module.exports = app;
